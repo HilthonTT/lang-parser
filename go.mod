bdfab46468b6fe98b5b92a3d4bca0166a7292751
Timothy Tandt <Timothy.tandt@outlook.be>
2026-03-08 21:32:50 +0100

    Tokenization

---
+++ go.mod
@@ -0,0 +1,3 @@
+module lang-parser
+
+go 1.26.1
---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"lang-parser/src/lexer"
+	"os"
+)
+
+func main() {
+	bytes, err := os.ReadFile("./examples/00.lang")
+	if err != nil {
+		panic(err)
+	}
+
+	source := string(bytes)
+	tokens := lexer.Tokenize(source)
+
+	for _, token := range tokens {
+		token.Debug()
+	}
+}
